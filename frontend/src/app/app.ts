b94fa56f4a2db8c087dd9ad677b6c3bafc573595
FBL <fabien.bardouil@gmail.com>
2026-03-30 11:03:28 +0200

    feat(frontend): add recommendation panel component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Chessboard } from './components/chessboard/chessboard';
+import { RecommendationPanel } from "./components/recommendation-panel/recommendation-panel";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Chessboard], // 👈 ajoute ça
+  imports: [Chessboard, RecommendationPanel],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
